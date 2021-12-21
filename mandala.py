6d7e4149150e1af2076bf108603496bfa66a9460
YourManSken <lorenzo.frittoli.ge@gmail.com>
2021-12-21 14:10:33 +0100

    Random mandala generation

--- mandala.py
+++ mandala.py
@@ -1,11 +1,14 @@
 import turtle
+import random
+
+turtle.speed(0)
 
 def regular_polygon(side_number, side_length):
     if (side_number > 1 and side_number%1 == 0):
         if (side_length > 0):
             angle = 360/side_number
             turtle.left(angle/2)
-            for l in range(side_number):
+            for _ in range(side_number):
                 turtle.forward(side_length)
                 turtle.right(angle)
             turtle.right(angle/2)
@@ -15,11 +18,11 @@ def regular_polygon(side_number, side_length):
     else:
         print('Invalid side number')
 
-def regular_polygon_circle(polygon_number, radius, side_number, side_length):
+def regular_polygon_circle(polygon_number, polygon_color,  radius, side_number, side_length):
     if (polygon_number > 0 and polygon_number%1 == 0):
         if (radius > 0):
             angle = 360/polygon_number
-            for p in range(polygon_number):
+            for _ in range(polygon_number):
                 turtle.penup()
                 turtle.forward(radius)
                 turtle.pendown()
@@ -32,6 +35,14 @@ def regular_polygon_circle(polygon_number, radius, side_number, side_length):
     else:
         print('Invalid polygon number')
 
-#regular_polygon(4, 100)
-regular_polygon_circle(20, 100, 4, 150)
-input()
+def mandala(stages):
+    for s in range(stages):
+        polygon_number = random.randint(2, 60)
+        polygon_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        radius = random.randrange(turtle.screensize()[1]*0.7/(stages-s))
+        side_number = random.randint(3, 8)
+        side_length = random.randint(10, 100)
+        regular_polygon_circle(polygon_number, polygon_color,  radius, side_number, side_length)
+
+mandala(2)
+turtle.exitonclick()
